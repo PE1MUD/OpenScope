e3c98749634d2593faf2b630d2cc1646f6cfaea8
David Roosendaal <pe1mud@hotmail.com>
2026-08-04 00:55:15 +0200

    Enable PAL input on DeckLink device

--- src/DeckLinkProbe.cpp
+++ src/DeckLinkProbe.cpp
@@ -5,6 +5,156 @@
 #include <QDebug>
 #include <QString>
 
+static void dumpConnections(const char* label, int64_t value)
+{
+    qDebug() << label;
+
+    if (value & bmdVideoConnectionSDI)
+        qDebug() << "    SDI";
+
+    if (value & bmdVideoConnectionHDMI)
+        qDebug() << "    HDMI";
+
+    if (value & bmdVideoConnectionOpticalSDI)
+        qDebug() << "    Optical SDI";
+
+    if (value & bmdVideoConnectionComponent)
+        qDebug() << "    Component";
+
+    if (value & bmdVideoConnectionComposite)
+        qDebug() << "    Composite";
+
+    if (value & bmdVideoConnectionSVideo)
+        qDebug() << "    S-Video";
+}
+
+static void dumpDisplayModes(IDeckLink* deckLink)
+{
+    IDeckLinkInput* input = nullptr;
+
+    if (deckLink->QueryInterface(
+        IID_IDeckLinkInput,
+        reinterpret_cast<void**>(&input)) != S_OK)
+    {
+        qDebug() << "  No capture interface";
+        return;
+    }
+
+    IDeckLinkDisplayModeIterator* modeIterator = nullptr;
+
+    if (input->GetDisplayModeIterator(&modeIterator) != S_OK)
+    {
+        qDebug() << "  Cannot enumerate display modes";
+        input->Release();
+        return;
+    }
+
+    qDebug() << "  Input modes:";
+
+    IDeckLinkDisplayMode* mode = nullptr;
+
+    while (modeIterator->Next(&mode) == S_OK)
+    {
+        BSTR name = nullptr;
+
+        if (mode->GetName(&name) == S_OK)
+        {
+            BMDTimeValue frameDuration = 0;
+            BMDTimeScale timeScale = 0;
+
+            mode->GetFrameRate(&frameDuration, &timeScale);
+
+            const double fps =
+                frameDuration != 0
+                ? static_cast<double>(timeScale) /
+                static_cast<double>(frameDuration)
+                : 0.0;
+
+            qDebug().noquote()
+                << QString("    %1 - %2x%3 - %4 fps")
+                .arg(QString::fromWCharArray(name))
+                .arg(mode->GetWidth())
+                .arg(mode->GetHeight())
+                .arg(fps, 0, 'f', 3);
+
+            SysFreeString(name);
+        }
+
+        mode->Release();
+    }
+
+    modeIterator->Release();
+    input->Release();
+}
+
+static void testPalInput(IDeckLink* deckLink)
+{
+    IDeckLinkInput* input = nullptr;
+
+    if (deckLink->QueryInterface(
+        IID_IDeckLinkInput,
+        reinterpret_cast<void**>(&input)) != S_OK)
+    {
+        qDebug() << "  No capture interface";
+        return;
+    }
+
+    const HRESULT result = input->EnableVideoInput(
+        bmdModePAL,
+        bmdFormat8BitYUV,
+        bmdVideoInputFlagDefault
+    );
+
+    if (SUCCEEDED(result))
+    {
+        qDebug() << "  PAL input enabled";
+        input->DisableVideoInput();
+    }
+    else
+    {
+        qDebug() << "  Failed to enable PAL input. HRESULT:"
+            << QString::number(
+                static_cast<unsigned long>(result), 16);
+    }
+
+    input->Release();
+}
+
+static void dumpDevice(IDeckLink* deckLink, int index)
+{
+    BSTR name = nullptr;
+
+    if (deckLink->GetDisplayName(&name) == S_OK)
+    {
+        qDebug() << index << ":"
+            << QString::fromWCharArray(name);
+
+        SysFreeString(name);
+    }
+
+    IDeckLinkProfileAttributes* attributes = nullptr;
+
+    if (deckLink->QueryInterface(IID_IDeckLinkProfileAttributes,
+        (void**)&attributes) == S_OK)
+    {
+        int64_t value = 0;
+
+        if (attributes->GetInt(BMDDeckLinkVideoInputConnections, &value) == S_OK)
+        {
+            dumpConnections("  Video inputs:", value);
+        }
+
+        if (attributes->GetInt(BMDDeckLinkVideoOutputConnections, &value) == S_OK)
+        {
+            dumpConnections("  Video outputs:", value);
+        }
+
+        attributes->Release();
+    }
+    dumpDisplayModes(deckLink);
+    testPalInput(deckLink);
+}
+
 void deckLinkProbe()
 {
     IDeckLinkIterator* iterator = nullptr;
@@ -29,17 +179,10 @@ void deckLinkProbe()
 
     while (iterator->Next(&deckLink) == S_OK)
     {
-        BSTR name = nullptr;
-
-        if (deckLink->GetDisplayName(&name) == S_OK)
-        {
-            qDebug() << index << ":"
-                << QString::fromWCharArray(name);
-
-            SysFreeString(name);
-        }
+        dumpDevice(deckLink, index);
 
         deckLink->Release();
+        deckLink = nullptr;
         ++index;
     }
 
